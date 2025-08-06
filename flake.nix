bb398ba9026e7ec6acdf9b62f5ed8abcee8f3ee3
pretofusia <preto.acc@mailbox.org>
2025-08-06 22:26:02 +0200

    Add flake.nix for NixOS config

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Preto's NixOS with Hyprland and flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    nixosConfigurations.preto = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.users.preto = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
