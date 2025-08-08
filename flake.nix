4fe76f34f4af7d3efbd49b7e0bd9cb5243218d3d
Preto <preto@install>
2025-08-08 23:57:06 +0200

    flake: pin home-manager to release-24.05 to match nixpkgs 24.05

--- flake.nix
+++ flake.nix
@@ -3,57 +3,41 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       lib = nixpkgs.lib;
-    in
-    {
-      nixosConfigurations = {
-        preto = lib.nixosSystem {
-          inherit system;
-          modules = [
-            # UEFI-Boot
-            ({ ... }: {
-              boot.loader.systemd-boot.enable = true;
-              boot.loader.efi.canTouchEfiVariables = true;
-            })
+    in {
+      nixosConfigurations.preto = lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./hosts/preto/hardware-configuration.nix
+          ./hosts/preto/configuration.nix
 
-            # Host-Module
-            ./hosts/preto/hardware-configuration.nix
-            ./hosts/preto/configuration.nix
+          home-manager.nixosModules.home-manager
+          ({ pkgs, ... }: {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            # Kein disabledModules/Override mehr nötig:
+            home-manager.users.preto = import ./home.nix;
+          })
 
-            # Home Manager einbinden + mako-Override
-            home-manager.nixosModules.home-manager
-            ({ pkgs, ... }: {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-
-              home-manager.users.preto = { ... }: {
-                # Upstream-HM-Modul für mako deaktivieren,
-                # stattdessen lokales Modul benutzen
-                disabledModules = [ "services/mako.nix" ];
-                imports = [
-                  ./home.nix
-                  ./hm-overrides/services/mako.nix
-                ];
-              };
-            })
-
-            # Overlay: pkgs.replaceVars als Alias für substituteAll bereitstellen
-            ({ ... }: {
-              nixpkgs.overlays = [
-                (final: prev: {
-                  replaceVars = file: vars: prev.substituteAll (vars // { src = file; });
-                })
-              ];
-            })
-          ];
-        };
+          # Overlay (falls du es brauchst) lassen wir drin:
+          ({ ... }: {
+            nixpkgs.overlays = [
+              (final: prev: {
+                replaceVars = file: vars: prev.substituteAll (vars // { src = file; });
+              })
+            ];
+          })
+        ];
       };
     };
 }
