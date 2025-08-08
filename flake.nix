53c4284c6bacdcc3367b1ded218ebdf2959b2aa0
Preto <preto@install>
2025-08-09 00:05:30 +0200

    flake: pin HM 24.05, systemd-boot, mako override

--- flake.nix
+++ flake.nix
@@ -1,43 +1,73 @@
 {
-  description = "NixOS configuration for preto";
+  description = "NixOS config (preto) with HM 24.05 and mako override";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
+    # HM passend zu 24.05
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-      lib = nixpkgs.lib;
-    in {
-      nixosConfigurations.preto = lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./hosts/preto/hardware-configuration.nix
-          ./hosts/preto/configuration.nix
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  let
+    system = "x86_64-linux";
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations.preto = lib.nixosSystem {
+      inherit system;
 
-          home-manager.nixosModules.home-manager
-          ({ pkgs, ... }: {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            # Kein disabledModules/Override mehr nötig:
-            home-manager.users.preto = import ./home.nix;
-          })
+      specialArgs = { inherit inputs; };
 
-          # Overlay (falls du es brauchst) lassen wir drin:
-          ({ ... }: {
-            nixpkgs.overlays = [
-              (final: prev: {
-                replaceVars = file: vars: prev.substituteAll (vars // { src = file; });
-              })
-            ];
-          })
-        ];
-      };
+      modules = [
+        # Dein bisheriger Host-Stack (Datei/Ordner muss es bei dir geben)
+        ./nixos
+
+        # Bootloader klar auf UEFI + systemd-boot festnageln (GRUB hart aus)
+        ({ lib, ... }: {
+          boot.loader.systemd-boot.enable = true;
+          boot.loader.efi.canTouchEfiVariables = true;
+          boot.loader.grub.enable = lib.mkForce false;
+
+          # nix / flakes QoL
+          nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+          # passt zur verwendeten Release
+          system.stateVersion = "24.05";
+        })
+
+        # Home Manager einhängen + mako-Override aktivieren
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.preto = {
+            # HM-Standardmodul für mako deaktivieren und unser Override laden
+            disabledModules = [ "services/mako.nix" ];
+            imports = [ ./hm-overrides/services/mako.nix ];
+
+            home.username = "preto";
+            home.homeDirectory = "/home/preto";
+            programs.home-manager.enable = true;
+
+            # Beispiel: mako einschalten; Settings kommen im INI-Format
+            services.mako = {
+              enable = true;
+              settings = {
+                # Trag hier deine Werte ein – z.B.:
+                # [global]
+                # sort = "newest-first";
+                # icon_path = "/usr/share/icons";
+              };
+            };
+
+            home.stateVersion = "24.05";
+          };
+        }
+      ];
     };
+  };
 }
